b51e22df78197a0ac5c234b2998c39228dc70a8b
Wol Vic John C. Lim <wolvicjohn@gmail.com>
2025-04-11 13:58:19 +0800

    [minutes]map] added interactive marker

--- lib/pages/map/alltreelocationpage.dart
+++ lib/pages/map/alltreelocationpage.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:manggatectv2/services/app_designs.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -111,6 +113,12 @@ class AllTreeLocationPageState extends State<AllTreeLocationPage> {
                               });
                               final data = doc.data() as Map<String, dynamic>;
                               final stage = data['stage'];
+                              final timestamp =
+                                  DateFormat('EEEE, MMMM dd, yyyy h:mm a')
+                                      .format(data['timestamp'].toDate());
+                              final String? imageUrl = data['imageUrl'];
+                              final String? stageImageUrl =
+                                  data['stageImageUrl'];
 
                               String iconPath;
                               switch (stage) {
@@ -134,10 +142,172 @@ class AllTreeLocationPageState extends State<AllTreeLocationPage> {
                                 point: location,
                                 width: 50.0,
                                 height: 50.0,
-                                child: Image.asset(
-                                  iconPath,
-                                  width: 40.0,
-                                  height: 40.0,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    showModalBottomSheet(
+                                      context: context,
+                                      backgroundColor: Colors.white,
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.vertical(
+                                            top: Radius.circular(20)),
+                                      ),
+                                      barrierColor: Colors.transparent,
+                                      builder: (context) {
+                                        return Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child:
+                                                        data['imageUrl'] != null
+                                                            ? ClipRRect(
+                                                                borderRadius:
+                                                                    BorderRadius
+                                                                        .circular(
+                                                                            8.0),
+                                                                child: CachedNetworkImage(
+                                                                    imageUrl: imageUrl!,
+                                                                    width: 100,
+                                                                    height: 100,
+                                                                    fit: BoxFit.cover,
+                                                                    placeholder: (context, url) => Container(
+                                                                        width: 100,
+                                                                        height: 100,
+                                                                        padding: const EdgeInsets.all(20),
+                                                                        decoration: BoxDecoration(
+                                                                          color:
+                                                                              Colors.white,
+                                                                          borderRadius:
+                                                                              BorderRadius.circular(20),
+                                                                        ),
+                                                                        child: AppDesigns.loadingIndicator()),
+                                                                    errorWidget: (context, url, error) => Container(
+                                                                          width:
+                                                                              60,
+                                                                          height:
+                                                                              60,
+                                                                          color: Colors
+                                                                              .grey
+                                                                              .shade200,
+                                                                          child:
+                                                                              const Icon(Icons.error_outline),
+                                                                        )),
+                                                              )
+                                                            : Container(
+                                                                width: 60,
+                                                                height: 60,
+                                                                decoration:
+                                                                    BoxDecoration(
+                                                                  color: Colors
+                                                                      .grey
+                                                                      .shade200,
+                                                                  borderRadius:
+                                                                      BorderRadius
+                                                                          .circular(
+                                                                              8.0),
+                                                                ),
+                                                                child: const Icon(
+                                                                    Icons
+                                                                        .image_not_supported_outlined),
+                                                              ),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child:
+                                                        data['stageImageUrl'] !=
+                                                                null
+                                                            ? ClipRRect(
+                                                                borderRadius:
+                                                                    BorderRadius
+                                                                        .circular(
+                                                                            8.0),
+                                                                child: CachedNetworkImage(
+                                                                    imageUrl: stageImageUrl!,
+                                                                    width: 100,
+                                                                    height: 100,
+                                                                    fit: BoxFit.cover,
+                                                                    placeholder: (context, url) => Container(
+                                                                        width: 100,
+                                                                        height: 100,
+                                                                        padding: const EdgeInsets.all(20),
+                                                                        decoration: BoxDecoration(
+                                                                          color:
+                                                                              Colors.white,
+                                                                          borderRadius:
+                                                                              BorderRadius.circular(20),
+                                                                        ),
+                                                                        child: AppDesigns.loadingIndicator()),
+                                                                    errorWidget: (context, url, error) => Container(
+                                                                          width:
+                                                                              60,
+                                                                          height:
+                                                                              60,
+                                                                          color: Colors
+                                                                              .grey
+                                                                              .shade200,
+                                                                          child:
+                                                                              const Icon(Icons.error_outline),
+                                                                        )),
+                                                              )
+                                                            : Container(
+                                                                width: 60,
+                                                                height: 60,
+                                                                decoration:
+                                                                    BoxDecoration(
+                                                                  color: Colors
+                                                                      .grey
+                                                                      .shade200,
+                                                                  borderRadius:
+                                                                      BorderRadius
+                                                                          .circular(
+                                                                              8.0),
+                                                                ),
+                                                                child: const Icon(
+                                                                    Icons
+                                                                        .image_not_supported_outlined),
+                                                              ),
+                                                  ),
+                                                ],
+                                              ),
+                                              const SizedBox(height: 10),
+                                              Text('$stage',
+                                                  style: const TextStyle(
+                                                      fontSize: 20,
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                              const SizedBox(height: 10),
+                                              const Divider(),
+                                              const Text('Uploaded on',
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.grey)),
+                                              Text(timestamp,
+                                                  style: const TextStyle(
+                                                      fontSize: 16)),
+                                              const Divider()
+                                            ],
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                  child: Image.asset(
+                                    iconPath,
+                                    width: 40.0,
+                                    height: 40.0,
+                                  ),
                                 ),
                               );
                             }).toList(),
